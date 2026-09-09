b24af1eb207b3585622ad580238f124d8ff78f8a
rasheemaa <rasheema.abdullah@outlook.com>
2026-09-09 07:15:16 -0400

    Add Google Analytics 4 tracking

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
+// Google Analytics 4
+const googleAnalyticsId = "G-C7XV3YJCZE";
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", googleAnalyticsId);
+
+const googleAnalyticsScript = document.createElement("script");
+googleAnalyticsScript.async = true;
+googleAnalyticsScript.src = `https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`;
+document.head.appendChild(googleAnalyticsScript);
+
 const menuButton = document.querySelector(".menu-toggle");
 const siteNav = document.querySelector(".site-nav");
 
